e43cc66344f9cc829d7ab4f58457785e571e2642
Vectari <mateusz.majer996@gmail.com>
2024-07-05 17:09:42 +0200

    feat: add new author, change designer

--- src/components/DevelopedBy.tsx
+++ src/components/DevelopedBy.tsx
@@ -2,20 +2,36 @@
 
 const DevelopedBy = () => {
   return (
-    <div className="mb-8 font-mono text-neutral-10 text-xs text-center">
-      <span>
-        Developed by{" "}
-        <a
-          href="https://github.com/flamrdevs"
-          className="inline-block transition duration-300 hover:-translate-y-px outline-none hover:text-primary-10 focus-visible:text-primary-10"
-          target="_blank"
-          aria-label="Developer"
-        >
-          {" "}
-          flamrdevs
-        </a>
-      </span>
-    </div>
+    <>
+      <div className="mb-8 font-mono text-neutral-10 text-xs text-center">
+        <span>
+          Developed by{" "}
+          <a
+            href="https://github.com/vectari"
+            className="inline-block transition duration-300 hover:-translate-y-px outline-none hover:text-primary-10 focus-visible:text-primary-10"
+            target="_blank"
+            aria-label="Developer"
+          >
+            {" "}
+            Vectari
+          </a>
+        </span>
+      </div>
+      <div className="mb-8 font-mono text-neutral-10 text-xs text-center">
+        <span>
+          Design by{" "}
+          <a
+            href="https://github.com/flamrdevs"
+            className="inline-block transition duration-300 hover:-translate-y-px outline-none hover:text-primary-10 focus-visible:text-primary-10"
+            target="_blank"
+            aria-label="Developer"
+          >
+            {" "}
+            flamrdevs
+          </a>
+        </span>
+      </div>
+    </>
   );
 };
 
