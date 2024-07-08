e8562fafab66e5b99f6e057a346b96db0f675fe3
Vectari <mateusz.majer996@gmail.com>
2024-07-08 16:45:36 +0200

    feat: add content and style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,66 +10,89 @@ export default () => {
         </Avatar>
 
         <div className="flex flex-col items-start gap-2">
-          <h1 className="font-medium text-lg lg:text-xl">
+          <h1 className="text-neutral-12 font-medium text-lg lg:text-xl">
             Ekspresowa Naprawa - Konsole i Kontrolery
           </h1>
-          <h2 className="font-medium text-2xl lg:text-3xl">CONsFix Serwis</h2>
+          <h2 className="text-neutral-12 font-medium text-2xl lg:text-3xl">
+            CONsFix Serwis
+          </h2>
         </div>
       </div>
 
       <div>
-        Naprawa konsol i kontrolerów to nasza pasja i specjalność! Jeśli Twój
-        sprzęt nie uruchamia się, ma problem z obrazem, czy cierpi na driftujące
-        analogi, jesteśmy tu, by pomóc.
+        <p className="text-neutral-12 ">
+          Naprawa konsol i kontrolerów to nasza pasja i specjalność! Jeśli Twój
+          sprzęt nie uruchamia się, ma problem z obrazem, czy cierpi na
+          driftujące analogi, jesteśmy tu, by pomóc.
+        </p>
         <br />
-        <br />
-        <h2>Co oferujemy:</h2>
-        <li>
+        <h2 className="text-neutral-12 font-medium text-lg lg:text-xl">
+          Co oferujemy:
+        </h2>
+        <li className="text-neutral-10">
           Bezpłatna Diagnoza: Szybka ocena problemu, abyś wiedział/a, co się
           dzieje z Twoim sprzętem.
         </li>
-        <li>Niskie Ceny: Konkurencyjne ceny i przejrzyste koszty naprawy.</li>
-        <li>
+        <li className="text-neutral-10">
+          Niskie Ceny: Konkurencyjne ceny i przejrzyste koszty naprawy.
+        </li>
+        <li className="text-neutral-10">
           Szybkie Naprawy: Dzięki doświadczeniu i wysokiej jakości częściom,
-          naprawiamy Twój sprzęt szybko i skutecznie.{" "}
+          naprawiamy Twój sprzęt szybko i skutecznie.
         </li>
-        <li>
+        <li className="text-neutral-10">
           Gwarancja na Naprawy: Oferujemy gwarancję na wszystkie przeprowadzone
-          naprawy.{" "}
+          naprawy.
         </li>
-        <h2>Usługi Naprawcze:</h2>
-        <li>
+        <br />
+        <h2 className="text-neutral-12 font-medium text-lg lg:text-xl">
+          Usługi Naprawcze:
+        </h2>
+        <li className="text-neutral-10">
           Konsole: PS4, PS5, XBOX ONE, XBOX Series S/X, Steam Deck, Nintendo
           Switch
         </li>
-        <li>
+        <li className="text-neutral-10">
           Kontrolery: Pad PS4, Pad PS5, Pad XBOX ONE, Pad XBOX Series, Nintendo
           Joy-Con
         </li>
-        <h2>Co naprawiamy: </h2>
-        <li>Naprawa problemów z uruchamianiem i wyświetlaniem obrazu </li>
-        <li>
-          Problemy z chłodzeniem: czyszczenie układu chłodzenia, wymiana past
-          termoprzewodzących{" "}
+        <br />
+        <h2 className="text-neutral-12 font-medium text-lg lg:text-xl">
+          Co naprawiamy:{" "}
+        </h2>
+        <li className="text-neutral-10">
+          Naprawa problemów z uruchamianiem i wyświetlaniem obrazu{" "}
         </li>
-        <li>Naprawa driftujących analogów </li>
-        <li>
+        <li className="text-neutral-10">
+          Problemy z chłodzeniem: czyszczenie układu chłodzenia, wymiana past
+          termoprzewodzących
+        </li>
+        <li className="text-neutral-10">Naprawa driftujących analogów </li>
+        <li className="text-neutral-10">
           Wymiana laserów, wyświetlaczy i innych uszkodzonych podzespołów{" "}
         </li>
-        <h2>Technologia HALL EFFECT:</h2>
-        <div>
+        <br />
+        <h2 className="text-neutral-12 font-medium text-lg lg:text-2xl">
+          Technologia HALL EFFECT:
+        </h2>
+        <p className="text-neutral-10 text-center">
           Na życzenie klienta oferujemy montaż nowoczesnych analogów HALL
           EFFECT, które są znacznie bardziej niezawodne.
-        </div>{" "}
-        <h2>
-          Kontaktuj się z nami! Dzięki naszemu wieloletniemu doświadczeniu w
-          naprawach elektroniki oraz profesjonalnemu podejściu, możesz być
-          pewny/pewna, że Twój sprzęt trafi w dobre ręce.{" "}
-        </h2>
-        <h2>
-          Skontaktuj się z nami już dziś, aby rozwiązać problemy ze swoją
-          konsolą lub kontrolerem!
-        </h2>
+        </p>
+        <br />
+        <br />
+        <div className="items-center justify-between px-4 py-3 bg-neutral-2  outline-none border border-neutral-4  rounded-3xl">
+          <h2 className="text-neutral-12 text-center font-medium text-lg lg:text-xl ">
+            Dzięki naszemu wieloletniemu doświadczeniu w naprawach elektroniki
+            oraz profesjonalnemu podejściu, możesz być pewny/pewna, że Twój
+            sprzęt trafi w dobre ręce.
+          </h2>
+          <br />
+          <h2 className="text-neutral-12 text-center font-medium text-lg lg:text-xl ">
+            Skontaktuj się z nami już dziś, aby rozwiązać problemy ze swoją
+            konsolą lub kontrolerem!
+          </h2>
+        </div>
       </div>
     </div>
   );
